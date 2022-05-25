90d99f44026067da556500913a8a2561ec5d24a0
Syoma <koshkin.syoma@gmail.com>
2022-05-25 17:01:46 +0300

    Title

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from PyQt6.QtWidgets import QApplication, QWidget
 def main():
     application = QApplication(sys.argv)
     widget = QWidget()
+    widget.setWindowTitle("BallGame")
     widget.show()
     sys.exit(application.exec())
 
