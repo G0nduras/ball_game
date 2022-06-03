553cf335f8bda900d3aace97d818fd2910e9469a
Pavel Koshkin <koshkin.pasha@gmail.com>
2022-06-03 13:57:11 +0300

    move Ball class to sepparate file

---
+++ ball.py
@@ -0,0 +1,62 @@
+from typing import Optional
+from PyQt6.QtCore import Qt, QPointF
+from PyQt6.QtGui import QPainter, QColor, QPen, QBrush, QVector2D
+
+
+class Ball:
+    def __init__(
+            self,
+            x: int,
+            y: int,
+            default_color: str,
+            hover_color: str,
+            circle_radius: int,
+            speed: float,
+    ):
+        self._center_position = QPointF(x, y)
+        self._default_color = default_color
+        self._hover_color = hover_color
+        self._circle_radius = circle_radius
+        self._speed = speed
+        self._center_target: Optional[QPointF] = None
+
+    def set_center_target(self, center_target: QPointF):
+        self._center_target = center_target
+
+    def draw(self, painter: QPainter, mouse_position: QPointF):
+        pen = QPen()
+        pen.setWidth(2)
+        painter.setPen(pen)
+        painter.setBrush(QBrush(QColor(self._get_circle_color(mouse_position)), Qt.BrushStyle.SolidPattern))
+        painter.drawEllipse(
+            self._center_position,
+            round(self._circle_radius),
+            round(self._circle_radius),
+        )
+
+    def _get_circle_color(self, mouse_position: QPointF) -> str:
+        if mouse_position is None:
+            return self._default_color
+
+        from_circle_to_mouse = QVector2D(self._center_position - mouse_position)
+        if from_circle_to_mouse.length() < self._circle_radius:
+            return self._hover_color
+        else:
+            return self._default_color
+
+    def move(self, shift: QPointF):
+        self._center_position += shift
+
+    def calculate_shift2(self) -> QPointF:
+        if self._center_target is None:
+            return QPointF(0, 0)
+        from_ball_to_target = QVector2D(self._center_target - self._center_position)
+        if from_ball_to_target.length() < self._speed:
+            return self._center_target - self._center_position
+        else:
+            move_direction = from_ball_to_target.normalized()
+            return move_direction.toPointF() * self._speed
+
+    def is_clicked(self, mouse_position: QPointF) -> bool:
+        vector = QVector2D(self._center_position - mouse_position)
+        return vector.length() <= self._circle_radius
